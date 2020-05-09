bf231af2cf8b8b8b68bdd21350095f735fe3654c
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2020-05-09 03:37:49 +0200

    Interceptor implemented Login structured as an own component Dummy home page created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, NgModule } from '@angular/core';
-import {LoginService} from './services/login.service';
+import {LoginService} from './login/login.service';
 import {Router} from '@angular/router';
 
 @Component({
@@ -10,28 +10,9 @@ import {Router} from '@angular/router';
 export class AppComponent {
   title = 'frontend-agent';
   password: string;
-  show = false;
+  show = true;
 
   constructor(private loginService: LoginService, private  router: Router) {
-  }
-
-
-  login() {
-    this.loginService.getUser(this.password)
-      .subscribe(
-        response => {
-          localStorage.setItem('jwt', response.token);
-          this.router.navigate(['home']);
-          alert('successful login');
-          this.show = false;
-        },
-        err => {
-          if (err.status === 400) {
-            alert('Wrong password');
-          } else if (err.status === 406 || err.status === 403) {
-            alert('Wrong password');
-          }
-        });
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from '../login/login.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  secretString: string;
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
   }
 
+  // poziva metodu u login servisu jer me mrzelo da pravim novi servis!!
+  getString() {
+    this.loginService.getSecretString()
+      .subscribe(
+        response => {
+          this.secretString = response;
+        },
+        err => {
+          alert('You must log in');
+        });
+  }
 }
